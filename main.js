1d4ee82b9cdefb79ae9e80152aa5b8e9cd8326fb
JelzaVelza <37868836+JelzaVelza@users.noreply.github.com>
2020-03-09 09:19:14 -0400

    Update main.js

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ let addTaskButton = document.getElementsByClassName("addtask-btn")[0],
 console.log('1');
 
 addTaskButton.addEventListener('click', function(){
-    let newTask = document.getElementById('new-task').value,
+    let newTask = newTaskTextField.value,
         newTaskMinified = newTask.replace(/\s/g,'');
     console.log(newTask);
     if (newTaskMinified == "" || newTaskMinified == null){
