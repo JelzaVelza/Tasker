3027afccf9670254fbdf402fc7d8bd5f59b08113
JelzaVelza <37868836+JelzaVelza@users.noreply.github.com>
2020-03-09 09:20:01 -0400

    Update main.js

--- main.js
+++ main.js
@@ -4,8 +4,7 @@ let addTaskButton = document.getElementsByClassName("addtask-btn")[0],
 console.log('1');
 
 addTaskButton.addEventListener('click', function(){
-    let newTask = newTaskTextField.value,
-        newTaskMinified = newTask.replace(/\s/g,'');
+    let newTask = newTaskTextField.value.replace(/\s/g,'');
     console.log(newTask);
     if (newTaskMinified == "" || newTaskMinified == null){
         alert("field cannot be empty");
