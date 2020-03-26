b39a0fa2f4f1d5fc10c997aceedf849319e92391
ivan hoinych <hoinych@gmail.com>
2020-03-26 10:48:52 +0200

    started adding learned things to real project

--- app.js
+++ app.js
@@ -1,2 +1,10 @@
-const app = require('express');
+const express = require('express');
+const bodyParser = require('body-parser');
+const path = require('path');
+
+const app = express();
+
+const port = 3200;
+app.listen(port);
+console.log(`Server is running on http://localhost:${port}`);
 
